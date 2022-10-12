41866c2dcec692e5a7e7834856de7c2bdd01433f
Sebastian Naso <sebastiann1@gmail.com>
2022-10-12 14:04:40 -0300

    CABA
    Agregado CABA Contacto Nostros

---
+++ JS/footer.js
@@ -0,0 +1,20 @@
+document.getElementById("footer-id").innerHTML =/*html*/ `
+<div class="socialnetwork">
+    <p>Seguinos:</p>
+    <a href="https://www.facebook.com"><i id="ico" class="fa-brands fa-facebook"></i> Facebook</a><br>
+    <a href="https://www.twitter.com"><i id="ico" class="fa-brands fa-twitter"></i> Twitter</a><br>
+    <a href="https://www.instagram.com"><i id="ico" class="fa-brands fa-instagram"></i> Instagram</a><br>
+    <a href="https://www.whatsapp.com"><i id="ico" class="fa-brands fa-whatsapp"></i> Whatsapp</a><br>
+    <a href="google.com"><i class="fa-brands fa-google"></i> Google</a><br>
+</div>
+<div id="footer-destino">
+    <p>Destinos:</p>
+    <a href="SUB\CABA.html">CABA</a><br>
+    <a href="SUB\Neuquen.html">Neuquén</a> <br>
+    <a href="SUB\Salta.html">Salta</a><br>
+</div>
+
+<div class="mapa">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13137.453868054601!2d-58.3798465!3d-34.5949695!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x45ea2bda502eecb!2sMinisterio%20de%20Turismo%20y%20Deportes%20de%20la%20Nacion!5e0!3m2!1ses-419!2sar!4v1665020299199!5m2!1ses-419!2sar" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+`
---
+++ JS/nav.js
@@ -0,0 +1,29 @@
+document.getElementById("nav-id").innerHTML=`
+<label for="boton" class="btn-menu"><i class="fa-solid fa-bars"></i></label>
+        <input id="boton" type="checkbox">
+        
+        <div class="menu">
+            
+            <div class="items">
+                <a href="../index.html">Inicio</a>
+            </div>
+            <div class="items">
+                <a href="../SUB/Nosotros.html">Nosotros</a>
+            </div>
+            <div class="items2 items">
+                <a href="#footer-destino">Destinos <i class="fa fa-caret-down"></i></a>
+                    <div class="submenu">
+                            <a href="../SUB/CABA.html">CABA</a>  
+                            <a href="../SUB/Neuquen.html">Neuquén</a> 
+                            <a href="../SUB/Salta.html">Salta</a> 
+                    </div>
+            </div>
+            <div class="items">
+                <a href="../SUB/Contacto.html">Contacto</a>
+            </div>
+            
+               
+        </div> 
+        
+        
+`
